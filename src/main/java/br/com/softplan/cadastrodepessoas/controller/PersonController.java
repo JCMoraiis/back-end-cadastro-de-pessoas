195073183ad17e20d007031b5a2a393a678bb10f
Júlio Oliveira <juliocesar_morais@outlook.com>
2020-03-30 05:07:04 -0300

    Adicionar o retorno 201 no método post

--- src/main/java/br/com/softplan/cadastrodepessoas/controller/PersonController.java
+++ src/main/java/br/com/softplan/cadastrodepessoas/controller/PersonController.java
@@ -1,10 +1,14 @@
 package br.com.softplan.cadastrodepessoas.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.softplan.cadastrodepessoas.controller.dto.PersonDTO;
 import br.com.softplan.cadastrodepessoas.controller.form.PersonForm;
@@ -24,8 +28,11 @@ public class PersonController {
 	}
 	
 	@PostMapping("/cadastrar")
-	public void register(@RequestBody PersonForm form) {
+	public ResponseEntity<PersonDTO> register(@RequestBody PersonForm form, UriComponentsBuilder uriBuilder) {
 		Person person = form.toConvert();
 		personRepository.save(person);
+		
+		URI uri = uriBuilder.path("/cadastrar/{id}").buildAndExpand(person.getId()).toUri();
+		return ResponseEntity.created(uri).body(new PersonDTO(person));
 	}
 }
